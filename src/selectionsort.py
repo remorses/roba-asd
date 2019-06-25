10b09a6b84d3e7bd840167ce9ec7cfc62379f7e1
remorses <beats.by.morse@gmail.com>
2019-06-25 11:16:06 +0200

    fixes

--- src/selectionsort.py
+++ src/selectionsort.py
@@ -3,10 +3,10 @@
 
 
 def selectionsort(arr):
-    for i in range(len(arr)):
+    for i in range(len(arr)-1):
         min = i
-        for j in range(i, len(arr)):
-            if j < min:
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min]:
                 min = j
-        arr[j], arr[min] = arr[min], arr[j]
+        arr[i], arr[min] = arr[min], arr[i]
         
