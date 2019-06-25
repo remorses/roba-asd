4d65c4112e8bab2c56133ce33fac43c77188b6c4
remorses <beats.by.morse@gmail.com>
2019-06-25 11:06:42 +0200

    made remover

--- src/cpp/lists.cpp
+++ src/cpp/lists.cpp
@@ -40,6 +40,35 @@ List insertAt(List list, int position, int e) {
 
 }
 
+template <class F>
+List removeIf(List l, F function) {
+    auto begin = l;
+    auto previous = l;
+    int i = 0;
+    while (l) {
+        if (function(l->info, i)) {
+            if (begin==l) {
+                auto toDelete = l;
+                l = l->next;
+                begin = l;
+                delete toDelete;
+            } else {
+                auto toDelete = l;
+                previous->next = l->next;
+                l = l->next;
+                delete toDelete;
+            }
+        }   
+        i += 1;
+        previous = l;
+        if (l) l = l->next;
+    }
+    return begin;
+    // throw "not found";
+}
+
+
+
 void print(List l) {
     while(l) {
         cout << l->info << ", ";
@@ -49,13 +78,30 @@ void print(List l) {
 }
 
 
+bool deleter(int e, int i) {
+    return e == 666;
+}
+
+
+int length(List l) {
+    int i = 0;
+    while (l) {
+        i += 1;
+        l = l->next;
+    }
+    return i;
+}
+
 int main() {
     List list = nullptr;
     for (int i = 0; i<20; i++) {
         list = insertAt(list, i, i);
     }
-    list = insertAt(list, 4, 666);
+    // list = insertAt(list, 4, 666);
     // list = insertAt(list, 16, 666);
-    // list = insertAt(list, 0, 666);
+    list = insertAt(list, length(list), 666);
     print(list);
+    list = removeIf(list, deleter);
+    print(list);
+    // cout << length(list) << endl;
 }
